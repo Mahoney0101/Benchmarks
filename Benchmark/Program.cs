f5c7085199994f2ac576b37c5d1df3ae62fa050c
jmahoney <jmahoney@continuumcommerce.com>
2022-02-02 19:51:13 +0000

    Initial commit

---
+++ Benchmark/Program.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using BenchmarkDotNet.Diagnosers;
+
+
+[MemoryDiagnoser]
+public class MemoryBenchmarkerDemo
+{
+    [Benchmark]
+    public void EscapeUri()
+    {
+#pragma warning disable SYSLIB0013 // Type or member is obsolete
+		_ = Uri.EscapeUriString("MERCHID");
+#pragma warning restore SYSLIB0013 // Type or member is obsolete
+    }
+
+    [Benchmark]
+    public void EscapeData()
+    {
+		_ = Uri.EscapeDataString("MERCHID");
+	}
+	static void Main(string[] args) => BenchmarkSwitcher.FromAssemblies(new[] { typeof(MemoryBenchmarkerDemo).Assembly }).Run(args);
+}
+
